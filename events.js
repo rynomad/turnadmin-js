779eeecc7ac2ee24792ca699ea87a1eedc338d24
Ryan Bennett <ryan@filament.com>
2018-08-01 18:55:56 -0600

    refactor event parsing

---
+++ events.js
@@ -0,0 +1,90 @@
+
+
+function debracket(string){
+  return string.match(/<\S*>/g).map(str => str.substr(1, str.length - 2))
+}
+
+function getMatch(string, regex){
+  let match = string.match(regex)
+  if (!match) return null
+  match = match[0]
+  return match
+}
+
+const events = {
+  client(string){
+    const event_match = getMatch(string, /New UDP endpoint.*remote addr.*/)
+    if (!event_match) return null
+    
+    const [realm] = debracket(string)
+  
+    return {
+      type : 'client',
+      data : {
+        realm,
+        ip : event_match.split(' ').pop().split(':')[0]
+      }
+    }
+  },
+  allocate(string){
+    const event_match = getMatch(string, /Local relay addr.*\n.*\n.*ALLOCATE processed, success/)
+    if (!event_match) return null
+
+    const [realm, user] = debracket(event_match[0])
+  
+    return {
+      type : 'allocate',
+      data : {
+        user,
+        realm
+      }
+    }
+  },
+  usage(string){
+    const event_match = getMatch(string, /usage: realm.* username=.* sb=.*/)
+    if (!event_match) return null
+
+    const [realm, user] = debracket(event_match[0])
+  
+    const rp = Number.parseInt(event_match.match(/rp=[0-9]*/)[0].split('=')[1])
+    const rb = Number.parseInt(event_match.match(/rb=[0-9]*/)[0].split('=')[1])
+    const sp = Number.parseInt(event_match.match(/sp=[0-9]*/)[0].split('=')[1])
+    const sb = Number.parseInt(event_match.match(/sb=[0-9]*/)[0].split('=')[1])
+  
+    return {
+      type : 'usage',
+      data : {
+        realm,
+        user,
+        rp, rb, sp, sb
+      }
+    }
+  },
+  disconnect(string){
+    const event_match = string.match(/closed.*user.*realm.*origin.*local.*remote.*/)
+    if (!event_match) return null
+
+    const [user, realm, origin] = debracket(event_match[0])  
+  
+    let ip = event_match.match(/remote \S*:/)[0]
+    ip = ip.split(' ').pop()
+    ip = ip.substr(0, ip.length - 1)
+  
+    let reason = event_match.match(/reason:.*/)[0].split(' ')
+    reason.shift()
+    reason = reason.join(' ')
+  
+    return {
+      type : 'disconnect',
+      data : {
+        user,
+        realm,
+        origin,
+        ip,
+        reason
+      }
+    }
+  }
+}
+
+module.exports = (string) => Object.keys(events).reduce((evt, type) => evt || events[type](string), null) || {}
--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const {execSync, spawn} = require('child_process')
 const {EventEmitter} = require('events')    
 const logs = require('fs-jetpack').cwd('/var/log/')
 const ts = require('tail-stream')
+const parseEvent = require('./events.js')
 
 
 function debracket(string){
   return string.match(/<\S*>/g).map(str => str.substr(1, str.length - 2))
+}
+
+function getMatch(string, regex){
+  let match = string.match(regex)
+  if (!match) return null
+  match = match[0]
+  return match
 }
 
 class TurnAdmin extends EventEmitter{
@@ -23,81 +31,6 @@ class TurnAdmin extends EventEmitter{
 
   static getLogFile(){
     return logs.path(logs.find({matching : `turn_${this.getPID()}*.log`}).pop())
-  }
-  
-  static parseLogEvent(buffer){
-    const string = buffer.toString().trim()
-    let event_match;
-    let event = {}
-    
-    if (event_match = string.match(/New UDP endpoint.*remote addr.*/)){
-      event_match = event_match[0]
-      const [realm] = debracket(string)
-  
-      event = {
-        type : 'client',
-        client : {
-          realm,
-          ip : event_match.split(' ').pop().split(':')[0]
-        }
-      }
-
-    } else if (event_match = string.match(/Local relay addr.*\n.*\n.*ALLOCATE processed, success/)){
-      event_match = event_match[0]
-      const [realm, user] = debracket(event_match)
-
-      event = {
-        type : 'allocate',
-        allocate : {
-          user,
-          realm
-        }
-      } 
-
-    } else if (event_match = string.match(/usage: realm.* username=.* sb=.*/)){
-      event_match = event_match[0]
-      const [realm, user] = debracket(event_match)
-
-      const rp = Number.parseInt(event_match.match(/rp=[0-9]*/)[0].split('=')[1])
-      const rb = Number.parseInt(event_match.match(/rb=[0-9]*/)[0].split('=')[1])
-      const sp = Number.parseInt(event_match.match(/sp=[0-9]*/)[0].split('=')[1])
-      const sb = Number.parseInt(event_match.match(/sb=[0-9]*/)[0].split('=')[1])
-
-      event = {
-        type : 'usage',
-        allocate : {
-          realm,
-          user,
-          rp, rb, sp, sb
-        }
-      }
-      
-    } else if (event_match = string.match(/closed.*user.*realm.*origin.*local.*remote.*/)){
-      event_match = event_match[0]
-      const [user, realm, origin] = debracket(event_match)  
-
-      let ip = event_match.match(/remote \S*:/)[0]
-      ip = ip.split(' ').pop()
-      ip = ip.substr(0, ip.length - 1)
-
-      let reason = event_match.match(/reason:.*/)[0].split(' ')
-      reason.shift()
-      reason = reason.join(' ')
-
-      event = {
-        type : 'disconnect',
-        disconnect : {
-          user,
-          realm,
-          origin,
-          ip,
-          reason
-        }
-      }
-
-    }
-
-    return event
   }
 
   constructor(){
@@ -120,7 +53,7 @@ class TurnAdmin extends EventEmitter{
 
     this.tail.on('data', (data) => {
       console.log('LOGFILE',data.toString())
-      const event = TurnAdmin.parseLogEvent(data)
+      const event = parseEvent(buffer.toString().trim())
 
       switch (event.type){
         case 'client':
