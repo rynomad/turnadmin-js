2de26c0c9f95e64239033a2d712524bbae7aab28
Ryan Bennett <ryan@filament.com>
2018-08-03 18:57:29 -0600

    >.< refresh token

--- token_receiver.js
+++ token_receiver.js
@@ -24,7 +24,7 @@ class TokenManager extends EventEmitter{
     console.log("requesting refresh", this.refresh_token)
     return new Promise((resolve, reject) => {
       request.get(
-        `https://steemconnect.com/api/oauth2/token?refresh_token=${this.refresh_token}&grant_type=refresh_token`,
+        `https://steemconnect.com/api/oauth2/token?refresh_token=${this.refresh_token}&grant_type=refresh_token&client_secret=${this.secret}`,
         (err, res, body) => {
           console.log("REFRESH RES", body)
         }
