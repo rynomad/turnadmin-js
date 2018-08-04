bb380a9bab5e14e43936b5eb87b47abd73ade238
Ryan Bennett <ryan@filament.com>
2018-08-03 21:43:06 -0600

    flow refactor

--- token_receiver.js
+++ token_receiver.js
@@ -3,30 +3,86 @@ const url = require('url')
 const {EventEmitter} = require('events')
 const fs = require('fs')
 const request = require('request')
+const DAY_MS = 1000 * 60 * 60 * 24
 
 class TokenManager extends EventEmitter{
-  constructor({username, code, secretpath, certpath, keypath}){
-    super()
-    this.username = username
-    this.code = code
-    this.secret = fs.readFileSync(secretpath).toString().trim()
-    this.cert = fs.readFileSync(certpath)
-    this.key = fs.readFileSync(keypath)
+  async static fromFile(filepath){
+    const mgr = new TokenManager(JSON.parse(fs.readFileSync(jsonfile).toString().trim()))
+    mgr.persist(filepath)
+  }
 
-    if (this.code){
-      this.requestToken()
-    } else {
-      this.listenForCode()
+  constructor({
+    username, 
+    certfile, 
+    keyfile, 
+    secret,
+    code, 
+    access_token, 
+    refresh_token, 
+    expires_at
+  }) {
+    super()
+    this._json = {
+      username,
+      code,
+      secret,
+      certfile,
+      keyfile,
+      access_token,
+      refresh_token,
+      expires_at,
+      port
     }
+  }
+
+  get serialize(){
+    return JSON.stringify(this._json, null, 4)
+  }
+
+  persist(jsonfile){
+    this.on('update', () => {
+      fs.writeFileSync(jsonfile, this.serialize)
+    })
+  }
+
+  async init(){
+    if (!this._json.code){
+      await this.listenForCode()
+    }
+
+    if (!this._json.access_token){
+      await this.requestToken()
+    }
+
+    await this.maybeRefreshToken()
+  }
+
+  async maybeRefreshToken(){
+    if (this._json.expires_at < (Date.now() + DAY_MS)){
+      await this.refreshToken()
+    }
+  }
+
+  async consumeToken({access_token, refresh_token, expires_in, username}){
+    if (access_token && (username === this.username) && expires_in && refresh_token){
+      this._json.access_token = access_token
+      this._json.expires_at = Date.now() + (expires_in * 1000)
+      this._json.refresh_token = refresh_token
+      return true
+    }
+
+    return false
   }
 
   async refreshToken(){
     console.log("requesting refresh", this.refresh_token)
     return new Promise((resolve, reject) => {
       request.get(
-        `https://steemconnect.com/api/oauth2/token?refresh_token=${this.refresh_token}&grant_type=refresh_token&client_secret=${this.secret}`,
+        `https://steemconnect.com/api/oauth2/token?refresh_token=${this._json.refresh_token}&grant_type=refresh_token&client_secret=${this._json.secret}`,
         (err, res, body) => {
-          console.log("REFRESH RES", body)
+          if (err) return reject(err)
+          if (!this.consumeToken(JSON.parse(body))) return reject(body)
+          resolve()
         }
       )
     })
@@ -36,46 +92,53 @@ class TokenManager extends EventEmitter{
     console.log("requesting token ", this.code, this.secret)
     return new Promise((resolve, reject) => {
       request.get(
-        `https://steemconnect.com/api/oauth2/token?code=${this.code}&client_secret=${this.secret}`,
+        `https://steemconnect.com/api/oauth2/token?code=${this._json.code}&client_secret=${this._json.secret}`,
         (err,res, body) => {
-          body = JSON.parse(body)
-          const {access_token, username, expires_in, refresh_token, ..._body} = body
-
-          if (access_token && (username === this.username) && expires_in && refresh_token){
-            this.access_token = access_token
-            this.expires_at = Date.now() + (expires_in * 1000)
-            this.refresh_token = refresh_token
-            resolve()
-          } else {
-            reject(body)
-          }
-          console.log("GOT RES", err, body)
+          if (err) return reject(err)
+          if (!this.consumeToken(JSON.parse(body))) return reject(body)
+          resolve()
         }
       )
     })
   }
 
-  listenForCode(){
-    this.server = https.createServer({
-      cert : this.cert,
-      key : this.key
-    },(request, response) => {
-      const {query : {code}} = url.parse(request.url, true)
-      console.log('got query', code)
-      if (code) {
-        this.code = code
-        this.requestToken(code)
-        response.statusCode = 200
-        response.end()
-      } else {
-        response.statusCode = 500
-        response.statusMessage = "Bad Request"
-        response.end()
-      }
+  async listenForCode(){
+    return new Promise((resolve, reject) => {
+      this.server = https.createServer({
+        cert : fs.readFileSync(this._json.certfile),
+        key : fs.readFileSync(this._json.keyfile)
+      },(request, response) => {
+        const {query : {code}} = url.parse(request.url, true)
+        console.log('got query', code)
+        if (code) {
+          this._json.code = code
+          response.statusCode = 200
+          response.end(() => {
+            this.server.close(() => {
+              resolve()
+            })
+          })
+        } else {
+          response.statusCode = 500
+          response.statusMessage = "Bad Request"
+          response.end()
+        }
+      })
+      this.server.listen(this._json.port || 4443)
     })
 
-    this.server.listen(4443)
+
   }
 }
 
 module.exports = TokenManager
+
+if (require.main === 'module'){
+  const filepath = process.argv.pop()
+  const mgr = TokenManager.fromFile(filepath)
+  mgr.init().then(res => {
+    console.log("token manager up", mgr.serialize)
+  }).catch(e => {
+    console.error(e)
+  })
+}
