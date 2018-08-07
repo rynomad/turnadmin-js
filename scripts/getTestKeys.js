2aa319f62d37b74ddde5571c8b4b9f8449b0160e
Ryan Bennett <ryan@filament.com>
2018-08-06 21:48:03 -0600

    add tests for steempay

---
+++ scripts/getTestKeys.js
@@ -0,0 +1,60 @@
+const http = require('http')
+const url = require("url")
+const steemconnect = require('sc2-sdk')
+const testdir = require('fs-jetpack').dir('test')
+
+console.log("TESTDIR", testdir.cwd())
+const request = require('request-promise-native')
+const template = {
+  "app_secret" : process.env.STEEM_APP_SECRET,
+  "app_name" : process.env.STEEM_APP_NAME,
+}
+
+if (!(template.app_secret && template.app_name)){
+  throw new Error('must set process.env.STEEM_APP_SECRET && process.env.STEEM_APP_NAME')
+}
+
+
+const api = steemconnect.Initialize({
+  "app": template.app_name,
+  "callbackURL": "http://localhost:4000",
+  "scope": [
+    "vote",
+    "offline",
+    "comment"
+  ]
+})
+
+const login_url = `${api.getLoginURL()}&response_type=code`
+
+const server = http.createServer(async (req, res) => {
+  console.log(req.url)
+  const {query : {code}} = url.parse(req.url, true)
+
+  const token_url = `https://steemconnect.com/api/oauth2/token?code=${code}&client_secret=${template.app_secret}`
+  if (!template.user_one){
+    template.user_one = JSON.parse(await request.get(token_url))
+    res.writeHead(302, {
+      Location : login_url
+    })
+  } else if (!template.user_two) {
+    template.user_two = JSON.parse(await request.get(token_url))
+    testdir.write('keys.json',template)
+    res.write('SUCCESS')
+    setTimeout(() => {
+      process.exit(0)
+    }, 5000)
+  }
+
+  res.end()
+
+  console.log(template)
+})
+
+
+server.listen(4000)
+
+
+console.log(`visit ${login_url} and log in with your two test users`)
+
+
---
+++ test/steempay.js
@@ -0,0 +1,148 @@
+const assert = require('assert')
+const request = require('request-promise-native')
+const crypto = require('crypto')
+const testdir = require('fs-jetpack').dir(__dirname)
+
+
+const SteemPay = require('../steempay.js')
+
+const randString = () => crypto.randomBytes(32).toString('hex')
+
+describe('Steempay', function(){
+  this.timeout(20000)
+  before(async () => {
+    const keys = testdir.read('keys.json', 'json')
+    if (!keys){
+      throw new Error(`Must run 'npm run get_test_keys'`)
+    }
+
+    const user1_tokens = JSON.parse(
+      await request(`https://steemconnect.com/api/oauth2/token?refresh_token=${keys.user_one.refresh_token}&grant_type=refresh_token&client_secret=${keys.app_secret}`) 
+    )
+    const user2_tokens = JSON.parse(
+      await request(`https://steemconnect.com/api/oauth2/token?refresh_token=${keys.user_two.refresh_token}&grant_type=refresh_token&client_secret=${keys.app_secret}`)
+    )
+
+    this.user1 = new SteemPay({
+      username : user1_tokens.username,
+      sc2 : {
+        app : keys.app_name,
+        callback_url : 'http://localhost:4000',
+        accessToken : user1_tokens.access_token,
+        scope : [
+          'vote',
+          'comment',
+          'offline'
+        ]
+      }
+    })
+
+    this.user2 = new SteemPay({
+      username : user2_tokens.username,
+      sc2 : {
+        app : keys.app_name,
+        callbackURL : 'http://localhost:4000',
+        accessToken : user2_tokens.access_token,
+        scope : [
+          'vote',
+          'comment',
+          'offline'
+        ]
+      }
+    })
+  })
+
+  it('has two unique users for testing', async () => {
+    assert(this.user1.username !== this.user2.username, `Must have two unique users to test flow, got '${this.user1.username}' and '${this.user2.username}'`)
+  })
+
+  it('generates keypair', () => {
+    assert(this.user1._keypair.publicKey)
+    assert(this.user2._keypair.publicKey)
+  })
+
+  it('initializes', async () => {
+    try {
+      await this.user1.init()
+    } catch (e) {
+      console.error("USER 1 INIT ERROR")
+      console.log(e)
+    }
+    try {
+      await this.user2.init()
+    } catch (e) {
+      console.error("USER 2 INIT ERROR")
+      console.log(e)
+    }
+
+    const user1pub = await this.user2.getUserPublicKey(this.user1.username)
+    const user2pub = await this.user1.getUserPublicKey(this.user2.username)
+    console.log(user1pub.toString('hex'), user2pub.toString('hex'))
+    console.log(this.user1._keypair.publicKey.toString('hex'), this.user2._keypair.publicKey.toString('hex'))
+
+    assert(user1pub.toString('hex') === this.user1._keypair.publicKey.toString('hex'), 'user1 public keys not equal')
+    assert(user2pub.toString('hex') === this.user2._keypair.publicKey.toString('hex'), 'user2 public keys not equal')
+  })
+
+  it('sends encrypted message', async () => {
+    await this.user1.replyEncrypted({
+      author : this.user2.username,
+      permlink : 'steempay-public-key',
+      reply : {
+        title : 'TEST ENCRYPTED PAYLOAD',
+        body : 'hello world'
+      }
+    })
+
+    const reply = (await this.user2.getEncryptedReplies({
+      permlink : 'steempay-public-key',
+      commentor : this.user1.username,
+      title : 'TEST ENCRYPTED PAYLOAD'
+    }))[0]
+
+    assert(reply)
+    assert(reply.body === 'hello world')
+  })
+
+  it('provides service', async () => {
+    const advertisement = await this.user1.postAdvertisement({body : "hello world service"})
+    console.log("POSTED ADVERTISEMENT")
+
+    this.user1.provideService({
+      advertisement,
+      provider : (request) => {
+        console.log("GOT PROVIDER REQUEST", request)
+      }
+    })
+
+    console.log("PROVIDING SERVICE")
+
+    const order_permlink = await this.user2.placeOrder({
+      author : this.user1.username,
+      permlink
+    })
+
+    console.log("PLACED ORDER", order_permlink)
+
+    const invoice = await this.user2.receiveInvoice({
+      permlink : order_permlink
+    })
+
+    console.log("GOT INVOICE", invoice)
+
+    await this.user2.payInvoice({
+      seller : this.user1.username,
+      invoice
+    })
+
+    console.log("PAID INVOICE")
+
+    const delivery = await this.user2.receiveDelivery({
+      seller : this.user1.username,
+      order : order_permlink
+    })
+
+    console.log("GOT DELIVERY", delivery)
+  })
+
+})
